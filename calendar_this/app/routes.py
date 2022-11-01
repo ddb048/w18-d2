149e068d5208445c85de205dbef717ea5ae192be
david <ddb048@gmail.com>
2022-11-01 13:29:55 -0700

    datetime

--- calendar_this/app/routes.py
+++ calendar_this/app/routes.py
@@ -1,6 +1,7 @@
 from flask import (Blueprint, render_template)
 import os
 import sqlite3
+import datetime
 
 bp = Blueprint("main", __name__, url_prefix="/")
 DB_FILE = os.environ.get("DB_FILE")
@@ -9,7 +10,16 @@ DB_FILE = os.environ.get("DB_FILE")
 def main():
     with sqlite3.connect(DB_FILE) as conn:
         curs = conn.cursor()
-        curs.execute("SELECT id, name, start_datetime, end_datetime
-                FROM appointments
-                ORDER BY start_datetime;")
-    return render_template('main.html')
+        curs.execute("SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;")
+        rows = curs.fetchall()
+        start = []
+        end = []
+        for apps in rows:
+            datetime_str1 = apps[2]
+            datetime_str2 = apps[3]
+            datetime_obj1 = datetime.strptime(datetime_str1,  '%Y-%m-%d %H:%M:%S')
+            datetime_obj2 = datetime.strptime(datetime_str2,  '%Y-%m-%d %H:%M:%S')
+            start.append(datetime_obj1.strftime("%H:%M"))
+            end.append(datetime_obj2.strftime("%H:%M"))
+        print(rows)
+    return render_template('main.html', rows=rows, start=start, end=end)
