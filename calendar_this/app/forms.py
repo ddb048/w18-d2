aed838659523d09297b581f4bbe4aa6d32fe908a
david <ddb048@gmail.com>
2022-11-01 16:30:35 -0700

    updated daily path

--- calendar_this/app/forms.py
+++ calendar_this/app/forms.py
@@ -1,13 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime
+
 
 class AppointmentForm(FlaskForm):
     name = StringField("name")
-    start_datetimeD = DateField('start_date', validators=[DataRequired()])
-    start_datetimeT = TimeField('start_time', validators=[DataRequired()])
-    end_datetimeD = DateField('start_date', validators=[DataRequired()])
-    end_datetimeT = TimeField('start_time', validators=[DataRequired()])
+    start_date = DateField('start_date', validators=[DataRequired()])
+    start_time = TimeField('start_time', validators=[DataRequired()])
+    end_date = DateField('end_date', validators=[DataRequired()])
+    end_time = TimeField('end_time', validators=[DataRequired()])
     description = TextAreaField('description', validators=[DataRequired()])
     private = BooleanField('private')
     submit = SubmitField('Save')
+
+    def validate_end_date(form, field):
+        start = datetime.combine(form.start_date.data, form.start_time.data)
+        end = datetime.combine(field.data, form.end_time.data)
+        if start >= end:
+            msg = "End date/time must come after start date/time"
+            raise ValidationError(msg)
+        if form.start_date.data != form.end_date.data:
+            msg = "End date must be on same day as start date"
+            raise ValidationError(msg)
--- calendar_this/app/routes.py
+++ calendar_this/app/routes.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, redirect)
 import os
 import sqlite3
 from datetime import datetime
@@ -7,12 +7,31 @@ from app.forms import AppointmentForm
 bp = Blueprint("main", __name__, url_prefix="/")
 DB_FILE = os.environ.get("DB_FILE")
 
-@bp.route('/')
+@bp.route("/")
 def main():
+    d = datetime.now()
+    return redirect(url_for(".daily", year=d.year, month=d.month, day=d.day))
+
+@bp.route('/<int:year>/<int:month>/<int:day>', methods=['GET', 'POST'])
+def daily():
     form = AppointmentForm()
+    if form.validate_on_submit():
+        print("test")
+        with sqlite3.connect(DB_FILE) as conn:
+            curs = conn.cursor()
+            curs.execute('''INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
+            VALUES (:name, :start_datetime, :end_datetime, :description, :private)''', {
+                'name': form.name.data,
+                'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+                'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+                'description': form.description.data,
+                'private': form.private.data})
+            conn.commit()
+            return redirect('/')
     with sqlite3.connect(DB_FILE) as conn:
         curs = conn.cursor()
         curs.execute("SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;")
+
         rows = curs.fetchall()
         appointments = []
         for apps in rows:
