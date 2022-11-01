d338e72d21c2d36d627a6d0a8bd068e9ca010451
david <ddb048@gmail.com>
2022-11-01 11:18:58 -0700

    up to datetime

---
+++ calendar_this/app/routes.py
@@ -0,0 +1,15 @@
+from flask import (Blueprint, render_template)
+import os
+import sqlite3
+
+bp = Blueprint("main", __name__, url_prefix="/")
+DB_FILE = os.environ.get("DB_FILE")
+
+@bp.route('/')
+def main():
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        curs.execute("SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;")
+    return render_template('main.html')
